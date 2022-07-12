5d2ad8cd8b151b1f499a361cbc1598591e883fa2
Moises Silva <moiss.silva@outlook.com>
2022-07-12 15:12:35 -0300

    SellerDaoJDBC and DaoFactory

--- src/app/ProgramaTesting.java
+++ src/app/ProgramaTesting.java
@@ -4,12 +4,16 @@ import java.util.Date;
 
 import entities.Department;
 import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class ProgramaTesting {
 
 	public static void main(String[] args) {
 		Department dep = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 
 	}
